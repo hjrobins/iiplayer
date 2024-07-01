48af57b7ed81cb467c52abc2d853b6377f7db611
hjrobins <52089587+hjrobins@users.noreply.github.com>
2024-07-01 23:36:42 +0100

    New version
    I have reduced the script in some ways and increased it in others. Added Detection for resolution, if the file is 1920x1080 it doesn't matter, if it's 960x540 and 16:9 then it'll upscale it to 1024x576 to interlace properly. If you're video is 4:3 within 16:9 frame then add the argument -4:3 and it'll crop it and make it 768x576.

--- iiplayer.py
+++ iiplayer.py
@@ -1,8 +1,6 @@
 import os
 import sys
 import subprocess
-import json
-import argparse
 
 def search_files(input_string, folder_path):
     matching_files = []
@@ -21,131 +19,71 @@ def display_matching_files(matching_files):
     for i, file_path in enumerate(matching_files, start=1):
         print(f"{i}. {file_path}")
 
-def get_size(selected_file):
-    #size_in_bytes = os.path.getsize(selected_file)
-    #size_mb = size_in_bytes / (1024 * 1024)  # Convert bytes to megabytes
-    #size_mb *= 1.05  # Add a 5% buffer
-    
-    ffprobe_cmd = f'ffprobe -hide_banner -v error -select_streams v:0 -show_entries stream=bit_rate -print_format json "{selected_file}"'
-    process = subprocess.Popen(ffprobe_cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE, shell=False)
-    print(selected_file)
-    out, err =  process.communicate()
-    if len(out)>0: # ** if result okay
-        print("==========output==========")
-        result = json.loads(out)
-        bitrate = result["streams"][0]["bit_rate"]
-        print(bitrate)
-    else:
-        result = {}
-    if err:
-        print("========= error ========")
-        print(err)
-    return f"-b:v {int(bitrate)/1000}k"
-   
-    
-def generate_ffmpeg_command(selected_file, bitrate, ffmpeg_output, extension, two_pass, interlaced, invert):
-    output_file = selected_file.replace(".mp4", f"_new.{extension}")
-    if two_pass == False:
-        return (
-              f'ffmpeg -hide_banner -i "{selected_file}" -filter_complex "'+f"[0:v]select='mod(n-1,2)'[top],[0:v]select='not(mod(n-1,2))'[bottom],[{invert[0]}]field=top[t],[{invert[1]}]field=bottom[b],[t][b]vstack,il=l=i:c=i"+f'{interlaced[0]}" -r 25 {ffmpeg_output} {bitrate} {interlaced[1]} "{output_file}"'
-        )
-    else:
-        one = f'ffmpeg -hide_banner -i "{selected_file}" -filter_complex "'+f"[0:v]select='mod(n-1,2)'[top],[0:v]select='not(mod(n-1,2))'[bottom],[{invert[0]}]field=top[t],[{invert[1]}]field=bottom[b],[t][b]vstack,il=l=i:c=i"+f'{interlaced[0]}" -r 25 {ffmpeg_output} {bitrate} {interlaced[1]} -pass 1 -f null /dev/null && '
-        two = f'ffmpeg -hide_banner -i "{selected_file}" -filter_complex "'+f"[0:v]select='mod(n-1,2)'[top],[0:v]select='not(mod(n-1,2))'[bottom],[{invert[0]}]field=top[t],[{invert[1]}]field=bottom[b],[t][b]vstack,il=l=i:c=i"+f'{interlaced[0]}" -r 25 {ffmpeg_output} {bitrate} {interlaced[1]} -pass 2 "{output_file}"'
-        return (one+two)
+def get_size(file_location):
+    size_in_bytes = os.path.getsize(file_location)
+    size_mb = size_in_bytes / (1024 * 1024)  # Convert bytes to megabytes
+    size_mb *= 1.05  # Add a 5% buffer
+    return int(size_mb)
 
-def main():
-    parser = argparse.ArgumentParser(description="A little tool to convert iPlayer 50p files to 25i/p")
-    parser.add_argument("input_string", help="Add the PID or filename")
-    parser.add_argument("-interlaced", action="store_true", help="Enable interlaced output TFF")
-    parser.add_argument("-interlacedbff", action="store_true", help="Enable interlaced output BFF")
-    parser.add_argument("-invert", action="store_true", help="Swaps to even frames for bottom fields etc")
-    args = parser.parse_args()
-    
-    inverted = args.invert
-    
-    input_string = args.input_string
-    invert = []
-    
-    if inverted == True:
-        invert = ["bottom", "top"]
-        print("invert is true")
-    else:
-        invert = ["top", "bottom"]
-        print("invert is false")
+def generate_staxrip_command(selected_file, size_mb):
+    return (
+        f'"C:\\Program Files\\StaxRip\\StaxRip.exe" '
+        f'-LoadTemplate:"iPlayer Interlace" "{selected_file}" -SetSize:{size_mb} -StartEncoding -ExitWithoutSaving'
+    )
 
-
-    if args.interlaced == True:
-        print("Interlace is True TFF")
-        interlaced = [",setfield=tff", "-flags +ildct"]
-    elif args.interlacedbff == True:
-        interlaced = [",setfield=bff", "-flags +ildct"]
-        print("Interlace is True BFF")
-    else:
-        interlaced = ["",""]    
-    
-    current_directory = os.path.dirname(os.path.abspath(__file__))
-    file_path = os.path.join(current_directory, "iiplayer.bat")
-    print(file_path)
+def delete_files(selected_file):
+    selected_file = os.path.splitext(selected_file)[0]
     try:
-        with open(file_path, "r") as file:
-            lines = file.readlines()
-            if len(lines) >= 3:
-                folder_path = lines[0].strip().split("; ", 1)[1]
-                folder_path = os.path.expandvars(folder_path)
-                ffmpeg_output = lines[1].strip().split("; ", 1)[1]
-                keep_bitrate = lines[2].strip().split("; ", 1)[1]
-                extension = lines[3].strip().split("; ", 1)[1]
-                two_pass = lines[4].strip().split("; ", 1)[1]
-                
-                if keep_bitrate.lower() == "true":
-                    keep_bitrate = True
-                else:
-                    keep_bitrate = False
-                    
-                if two_pass.lower() == "true":
-                    two_pass = True
-                elif two_pass.lower() == "false":
-                    two_pass = False
-                else:
-                    print("two_pass not set")
-    
-                print(f"Folder path: {folder_path}")
-                print(f"FFmpeg output command: {ffmpeg_output}")
-                print(f"Keep original bitrate: {keep_bitrate}")
-                print(f"Output extension: {extension}")
-                print(f"Output extension: {two_pass}")
-            else:
-                print("default file needs five lines. iPlayer recording folder. ffmpeg command. Keep Bitrate. File extension. Two pass")
+        shutil.rmtree(selected_file)  # Remove the entire directory tree
     except FileNotFoundError:
-        print("You're not running this using iiplayer.bat")
-    
+        # Handle the case where the directory does not exist
+        pass
+    try:
+        os.remove(selected_file + ".mp4")  # Remove the .mp4 file
+    except FileNotFoundError:
+        # Handle the case where the .mp4 file does not exist
+        pass
+    try:
+        os.rename(selected_file + "_new.mp4", selected_file + ".mp4")
+        return f"File renamed to {selected_file}.mp4""
+    except FileNotFoundError:
+        return "File not found"
+        
+        
+        
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <input_string>")
+        sys.exit(1)
+
+    input_string = sys.argv[1]
+    folder_path = r"C:\Users\Harry\Desktop\iPlayer Recordings"
 
     found_files = search_files(input_string, folder_path)
     display_matching_files(found_files)
-    
+
     if found_files:
         if len(found_files) == 1:
             selected_file = found_files[0]
-            
-            if keep_bitrate == True:
-                bitrate = get_size(selected_file)
-            else:
-                bitrate = ""
-        
-            ffmpeg_cmd = generate_ffmpeg_command(selected_file, bitrate, ffmpeg_output, extension, two_pass, interlaced, invert)
-            print(f"ffmpeg command:\n{ffmpeg_cmd}")
-            subprocess.run(ffmpeg_cmd, shell=True)
+            size_mb = get_size(selected_file)
+            print(f"Size: {size_mb:.2f} MB")
+
+            stax_cmd = generate_staxrip_command(selected_file, size_mb)
+            print(f"StaxRip command:\n{stax_cmd}")
+            subprocess.run(stax_cmd, shell=True)
+            delete_files(selected_file)
         else:
             try:
                 choice = int(input("Enter the number corresponding to the desired file: "))
                 if 1 <= choice <= len(found_files):
                     selected_file = found_files[choice - 1]
-                    bitrate = get_size(selected_file)
+                    size_mb = get_size(selected_file)
+                    print(f"Size: {size_mb:.2f} MB")
 
-                    ffmpeg_cmd = generate_ffmpeg_command(selected_file, bitrate, ffmpeg_output, extension, two_pass, interlaced, invert)
-                    print(f"ffmpeg command:\n{ffmpeg_cmd}")
-                    subprocess.run(ffmpeg_cmd, shell=True)
+                    stax_cmd = generate_staxrip_command(selected_file, size_mb)
+                    print(f"StaxRip command:\n{stax_cmd}")
+                    subprocess.run(stax_cmd, shell=True)
+                    delete_files(selected_file)
                 else:
                     print("Invalid choice. Please enter a valid number.")
             except ValueError:
